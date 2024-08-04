de6d604da13746a6d6bf5a8d66f1558ae8a74482
WENBBBY <145891074+WENBBBY@users.noreply.github.com>
2024-08-04 15:32:53 +0800

    Initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,16 @@
+public class Calculator {
+
+    private int x = 50;
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a*b;
+    }
+}
